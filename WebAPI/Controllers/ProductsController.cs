25a121639e7b2b716484c18e06fbaad88ef12b40
zeynepbaykal <55082321+zeynepbaykal@users.noreply.github.com>
2022-02-04 14:49:45 +0300

    code refactoring

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -32,7 +32,7 @@ namespace WebAPI.Controllers
             //swager
             //Dependency chain-- bağımlılık zinciri
 
-            Thread.Sleep(5000);
+            Thread.Sleep(1000);
 
             var result =_productService.GetAll();
             if (result.Success)
@@ -54,6 +54,17 @@ namespace WebAPI.Controllers
             return BadRequest(result);
         }
 
+        [HttpGet("getbycategory")]
+
+        public IActionResult GetByCategory(int categoryId)
+        {
+            var result = _productService.GetAllByCategoryId(categoryId);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
 
         [HttpPost("add")]
         public IActionResult Add(Product product)
